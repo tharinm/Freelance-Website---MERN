b8eeb4528814ec33ac85cb81aa961986f54e9771
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-04-14 02:00:20 +0530

    creat navebar change effect when scrolling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
-import React from "react"
-import Navbar from "./components/navbar/Navbar"
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
-
   return (
     <div>
-      <Navbar/>
+      <Navbar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.scss";
 
 export default function Navbar() {
+
+  const [active, setActive] = useState(false);
+  console.log(setActive)
+
+  const isActive = () =>
+  {
+    window.scrollY>0 ?setActive(true):setActive(false)
+    }
+
+  useEffect(() => {
+    window.addEventListener('scroll', isActive);
+      return () => {
+        window.removeEventListener('scroll',isActive)
+      }
+  },[])
+
   return (
-    <div className="navbar">
+    <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
+          {/* <Link to='/'> */}
           <span className="text">Freelance</span>
+          {/* </Link> */}
+
           <span className="dot">.</span>
         </div>
         <div className="links">
@@ -18,6 +37,15 @@ export default function Navbar() {
           <button>Join</button>
         </div>
       </div>
+
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Test</span>
+          </div>
+        </>
+      )}
     </div>
   );
 }
