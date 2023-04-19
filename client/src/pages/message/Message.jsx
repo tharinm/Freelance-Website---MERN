ec928024f2070bfcf0565689f3a4a38c77c227ab
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-04-19 17:57:43 +0530

    developed chat section

--- client/src/pages/message/Message.jsx
+++ client/src/pages/message/Message.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from "react";
+import "./Message.scss";
+import { Link } from "react-router-dom";
 
 export default function Message() {
   return (
-    <div>
-      Single Message
+    <div className="message">
+      <div className="container">
+        <span className="breadcrums">
+          <Link to="/messages" className="link">
+            MESSAGES By JOHN
+          </Link>
+        </span>
+        <div className="mesages">
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Do mollit ad ea esse ex Lorem. Sint consectet ur ipsum voluptate
+              cillum commodo irure cupidatat ani ur laboris Lorem quis cillum
+              exercitation cupidatat cupidatat velit minim occaecat nisi
+              laborum.
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Do mollit ad ea esse ex Lorem. Sint consectet ur ipsum voluptate
+              cillum commodo irure cupidatat ani ur laboris Lorem quis cillum
+              exercitation cupidatat cupidatat velit minim occaecat nisi
+              laborum.
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Do mollit ad ea esse ex Lorem. Sint consectet ur ipsum voluptate
+              cillum commodo irure cupidatat ani ur laboris Lorem quis cillum
+              exercitation cupidatat cupidatat velit minim occaecat nisi
+              laborum.
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Do mollit ad ea esse ex Lorem. Sint consectet ur ipsum voluptate
+              cillum commodo irure cupidatat ani ur laboris Lorem quis cillum
+              exercitation cupidatat cupidatat velit minim occaecat nisi
+              laborum.
+            </p>
+          </div>
+        </div>
+        <hr/>
+        <div className="write">
+          <textarea
+            name=""
+            placeholder="write message"
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button>Send</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
