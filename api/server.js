bcf75c8700f8ef506eba90ecddc177861aa84430
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-04-20 17:33:54 +0530

    mongodb connected with env

--- api/server.js
+++ api/server.js
@@ -1,7 +1,22 @@
+import e from "express";
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+//mongoose.set('strictQuery',true)
 const app = express();
+dotenv.config()
+
+
+try {
+  await mongoose.connect(process.env.MONGO);
+  console.log("MongoDB connected");
+} catch (error) {
+ console.log(error)
+}
+
+
 
 app.listen(8000, () => {
-    console.log("Backend server is running!");
-})
+  console.log("Backend server is running!");
+});
