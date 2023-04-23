752533432724472ab5d8ca303582b587a7cf254c
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-04-23 15:54:09 +0530

    implement jwt token

--- api/controller/user.contoller.js
+++ api/controller/user.contoller.js
@@ -1,3 +1,13 @@
-export const deleteUser = (req, res, next) => {
-   
-}
+import User from "../models/user.model.js";
+
+
+export const deleteUser = async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (req.userId !== user._id.toString()) {
+    return res.status(401).send("You can delete only your account!");
+  }
+
+  await User.findByIdAndDelete(req.params.id);
+  res.status(200).send("deleted");
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,12 @@
 import express from "express";
+import { deleteUser } from "../controller/user.contoller.js";
+import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
 
 router.get("/register");
 router.get("/login");
+
+router.delete("/:id", verifyToken, deleteUser);
 
 export default router;
