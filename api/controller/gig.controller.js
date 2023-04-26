e9221680279f1e329105665d7aadbb1995e428a7
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-04-26 12:02:19 +0530

    delete gig developed

--- api/controller/gig.controller.js
+++ api/controller/gig.controller.js
@@ -2,7 +2,6 @@ import gigModel from "../models/gig.model.js";
 import createError from "../utils/createError.js";
 import jwt from "jsonwebtoken";
 
-
 export const createGig = async (req, res, next) => {
   if (!req.isSeller)
     return next(createError(403, "Only seller can create gig"));
@@ -20,8 +19,20 @@ export const createGig = async (req, res, next) => {
   }
 };
 
-// export const deleteGig = () => {};
-//
+export const deleteGig = async (req, res, next) => {
+  try {
+    const gig = await gigModel.findById(req.params.id);
+    if (gig.userId == req.userId) {
+      await gigModel.findByIdAndDelete(req.params.id);
+      return res.status(200).send("deleted");
+    } else {
+      return next(createError(403, "You can delete your gig only"));
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 // export const getGig = () => {};
 //
 // export const getGigs = () => {};
--- api/routes/gig.route.js
+++ api/routes/gig.route.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import { verifyToken } from "../middleware/jwt.js";
-import {createGig} from '../controller/gig.controller.js'
+import {createGig,deleteGig} from '../controller/gig.controller.js'
 
 const router = express.Router();
 
 router.post("/", verifyToken, createGig);
-// router.delete("/:id", verifyToken, deleteGig);
+router.delete("/:id", verifyToken, deleteGig);
 // router.get("/single/:id", verifyToken, getGig);
 // router.get("/", verifyToken, getGigs);
 
